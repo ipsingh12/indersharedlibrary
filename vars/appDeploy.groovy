a0f8035d3c8b3e57dde36d49fa555db1d85c5472
ipsingh12 <ips.genius@gmail.com>
2018-11-03 23:11:14 +0530

    Update appDeploy.groovy

--- vars/appDeploy.groovy
+++ vars/appDeploy.groovy
@@ -2,7 +2,7 @@ def call() {
     pipeline {
         agent any
         stages {
-            stage('Even Stage') {
+            stage('Checkout') {
                 steps {
                     checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: '918d9575-eca1-40b3-9296-9cbf262a05de', url: 'https://github.com/ipsingh12/spring-petclinic.git']]])
                 }
