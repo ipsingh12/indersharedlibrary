2059e031bc049583f5c05e6f254ca044670f70ee
ipsingh12 <ips.genius@gmail.com>
2018-11-03 23:09:44 +0530

    Update appDeploy.groovy

--- vars/appDeploy.groovy
+++ vars/appDeploy.groovy
@@ -4,8 +4,7 @@ def call() {
         stages {
             stage('Even Stage') {
                 steps {
-                    checkout 
-                      scm: [$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: '918d9575-eca1-40b3-9296-9cbf262a05de', url: 'https://github.com/ipsingh12/spring-petclinic.git']]]
+                    checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: '918d9575-eca1-40b3-9296-9cbf262a05de', url: 'https://github.com/ipsingh12/spring-petclinic.git']]])
                 }
             }
         }
