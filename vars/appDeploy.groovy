3d2267e5c17cf392dc62e069962758cdf7a2750e
ipsingh12 <ips.genius@gmail.com>
2018-11-03 22:54:54 +0530

    Update appDeploy.groovy

--- vars/appDeploy.groovy
+++ vars/appDeploy.groovy
@@ -4,7 +4,8 @@ def call() {
         stages {
             stage('Even Stage') {
                 steps {
-                    echo "The build number is even"
+                    checkout 
+                      scm: [$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: '918d9575-eca1-40b3-9296-9cbf262a05de', url: 'https://github.com/ipsingh12/spring-petclinic.git']]]
                 }
             }
         }
